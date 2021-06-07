41de7f86d379bb20e58f61f55e4de1b01fa48b94
Ismail Dawoodjee <ismail.dawoodjee27@gmail.com>
2021-06-07 13:34:53 +0800

    Tested DynamicArray functions

--- Main.java
+++ Main.java
@@ -4,6 +4,23 @@
 public class Main {
 
     public static void main(String[] args) {
+        DynamicArray<Integer> array = new DynamicArray<Integer>(5);
+        array.add(5);
+        array.add(3);
+        array.add(-4);
+        array.add(-12);
+        System.out.println(array.size());
+        System.out.println(array.isEmpty());
+        System.out.println(array.get(1));
+        array.set(2, -9);
+        System.out.println(array.pop(3));
+        System.out.println(array.remove(5));
+        System.out.println(array.contains(-4));
+        System.out.println(array.indexOf(-4));
+        System.out.println(array);
+        array.clear();
+        System.out.println(array);
+
         
     }
 }
